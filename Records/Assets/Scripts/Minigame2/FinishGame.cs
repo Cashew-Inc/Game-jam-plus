1671005044a4b593723c72cd936e65858edfbe90
Davi Souza <dsl@ic.ufal.br>
2024-10-20 12:44:50 -0300

    :sparkles: feat: replaced tile set and winning sound on game 2

--- Records/Assets/Scripts/Minigame2/FinishGame.cs
+++ Records/Assets/Scripts/Minigame2/FinishGame.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class FinishGame : MonoBehaviour
 {
     public GameObject winCanvas;
+    public AudioSource winAudioSource;  // Referência ao AudioSource
     private bool hasWon = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !hasWon)
@@ -16,6 +18,10 @@ public class FinishGame : MonoBehaviour
     {
         hasWon = true;
         winCanvas.SetActive(true);
+        if (winAudioSource != null)  // Certifique-se de que há um AudioSource configurado
+        {
+            winAudioSource.Play();  // Toca o áudio ao ganhar o jogo
+        }
     }
 
     public bool getHasWon()
